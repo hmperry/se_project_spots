1a937b3a8998775d604b20f103cf35c95b4eb907
McKay Perry <hmperry17@gmail.com>
2025-06-17 15:42:34 -0600

    refactor api methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,68 +16,132 @@ class Api {
     return Promise.reject(`Error: ${res.status}`);
   }
 
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse);
+  }
+
   getAppInfo() {
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
+  // getUserInfo() {
+  //   return fetch(`${this._baseUrl}/users/me`, {
+  //     method: "GET",
+  //     headers: this._headers,
+  //   }).then(this._checkResponse);
+  // }
+
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: "GET",
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
+
+  // getInitialCards() {
+  //   return fetch(`${this._baseUrl}/cards`, {
+  //     method: "GET",
+  //     headers: this._headers,
+  //   }).then(this._checkResponse);
+  // }
 
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: "GET",
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
 
+  // addNewCard({ name, link }) {
+  //   return fetch(`${this._baseUrl}/cards`, {
+  //     method: "POST",
+  //     headers: { ...this._headers, "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       name,
+  //       link,
+  //     }),
+  //   }).then(this._checkResponse);
+  // }
+
   addNewCard({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: { ...this._headers, "Content-Type": "application/json" },
       body: JSON.stringify({
         name,
         link,
       }),
-    }).then(this._checkResponse);
+    });
   }
 
   deleteCard(id) {
-    return fetch(`${this._baseUrl}/cards/${id}`, {
+    return this._request(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
       headers: { ...this._headers, "Content-Type": "application/json" },
-    }).then(this._checkResponse);
+    });
   }
+
+  // deleteCard(id) {
+  //   return fetch(`${this._baseUrl}/cards/${id}`, {
+  //     method: "DELETE",
+  //     headers: { ...this._headers, "Content-Type": "application/json" },
+  //   }).then(this._checkResponse);
+  // }
 
   changeLike(id, isLiked) {
-    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+    return this._request(`${this._baseUrl}/cards/${id}/likes`, {
       method: isLiked ? "DELETE" : "PUT",
       headers: { ...this._headers, "Content-Type": "application/json" },
-    }).then(this._checkResponse);
+    });
   }
 
+  // changeLike(id, isLiked) {
+  //   return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+  //     method: isLiked ? "DELETE" : "PUT",
+  //     headers: { ...this._headers, "Content-Type": "application/json" },
+  //   }).then(this._checkResponse);
+  // }
+
   editUserInfo({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: { ...this._headers, "Content-Type": "application/json" },
       body: JSON.stringify({
         name,
         about,
       }),
-    }).then(this._checkResponse);
+    });
   }
 
+  // editUserInfo({ name, about }) {
+  //   return fetch(`${this._baseUrl}/users/me`, {
+  //     method: "PATCH",
+  //     headers: { ...this._headers, "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       name,
+  //       about,
+  //     }),
+  //   }).then(this._checkResponse);
+  // }
+
+  // changeProfilePicture({ avatar }) {
+  //   return fetch(`${this._baseUrl}/users/me/avatar`, {
+  //     method: "PATCH",
+  //     headers: { ...this._headers, "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       avatar,
+  //     }),
+  //   }).then(this._checkResponse);
+  // }
+
   changeProfilePicture({ avatar }) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    return this._request(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
       headers: { ...this._headers, "Content-Type": "application/json" },
       body: JSON.stringify({
         avatar,
       }),
-    }).then(this._checkResponse);
+    });
   }
 }
 
